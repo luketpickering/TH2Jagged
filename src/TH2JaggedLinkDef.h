c3afe2c60c6bfdd5ae4525596a3b846d2bd62134
Luke Pickering <luketpickering@googlemail.com>
2022-02-03 21:24:51 +0000

    Adds missing classes to the linkdef

--- src/TH2JaggedLinkDef.h
+++ src/TH2JaggedLinkDef.h
@@ -4,8 +4,12 @@
 #pragma link off all functions;
 #pragma link C++ nestedclasses;
 
+#pragma link C++ class JBinId;
+#pragma link C++ class std::map<JBinId, Int_t>;
+
 #pragma link C++ class TH2Jagged<double>;
 #pragma link C++ class TH2Jagged<float>;
 #pragma link C++ class TH2Jagged<int>;
 #pragma link C++ class TH2Jagged<char>;
+
 #endif
